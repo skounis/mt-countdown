453206cd3ea97af400094e4fdcc7de396b0418bf
Vasileios Kechagias <vkechagias@gmail.com>
2018-11-05 17:28:15 +0200

    fix(initialization js): search in context for element and only initialise once
    Also remove JS prepend to body as it is not required

--- modules/mt_countdown/js/mt_countdown.js
+++ modules/mt_countdown/js/mt_countdown.js
@@ -4,24 +4,18 @@
  */
 (function ($, Drupal, drupalSettings) {
 
-  Drupal.behaviors.mt_countdown = {
+  Drupal.behaviors.mtCountdown = {
     attach: function (context, settings) {
-
-      // Add html element with class "mt-count-down" after the body.
-      $(document).ready(function() {
-        $('.mt-count-down-inject').prependTo('body');
-      });
-
-      // Get access setting from 'drupalSettings'.
-      var expiration_date = drupalSettings.mt_countdown.expiration_date;
-
-      // Start countdown script.
-      $(".mt-count-down-slot").countdown(expiration_date, function(event) {
-        $(this).html(event.strftime('' +
-          '<span class="mt-time"><span class="mt-count">%D</span> <span class="mt-label">day%!d</span></span> ' +
-          '<span class="mt-time"><span class="mt-count">%H</span> <span class="mt-label">hours</span></span> ' +
-          '<span class="mt-time"><span class="mt-count">%M</span> <span class="mt-label">minutes</span></span> ' +
-          '<span class="mt-time"><span class="mt-count">%S</span> <span class="mt-label">seconds</span></span>'));
+      $(context).find('.mt-count-down-slot').once('mtCountdownInit').each(function() {
+        var expiration_date = drupalSettings.mt_countdown.expiration_date;
+        // Start countdown script.
+        $(this).countdown(expiration_date, function(event) {
+          $(this).html(event.strftime('' +
+            '<span class="mt-time"><span class="mt-count">%D</span> <span class="mt-label">day%!d</span></span> ' +
+            '<span class="mt-time"><span class="mt-count">%H</span> <span class="mt-label">hours</span></span> ' +
+            '<span class="mt-time"><span class="mt-count">%M</span> <span class="mt-label">minutes</span></span> ' +
+            '<span class="mt-time"><span class="mt-count">%S</span> <span class="mt-label">seconds</span></span>'));
+        });
       });
     }
   };
